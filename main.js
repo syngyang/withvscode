93f93d23e1040a62d9fe492bd9914b3fe69564d0
syngyang <syngyang@gmail.com>
2020-04-08 12:20:53 +0900

    DaeshinSite 1

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+
+
+ const card = document.querySelector('.card');
+// const zae = document.querySelector('.zae');
+// const autogard = document.querySelector('.autogard');
+// const planetoll = document.querySelector('.planetoll');
+// const companies = document.querySelector('.companies');
+
+
+
+card.addEventListener('mouseenter', ()=> {
+    this.remove;
+    
+ });
+// motrona.addEventListener('mouseleave', ()=> {
+//     item.classList.remove('hover-motrona');
+
+// });
+
+// right.addEventListener('mouseenter', ()=> {
+//     companies.classList.add('hover-right');
+// });
+// right.addEventListener('mouseleave', ()=> {
+//     companies.classList.remove('hover-right');
+// });
